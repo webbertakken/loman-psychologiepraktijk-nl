1a7e5edeb36344d017e4c157510a91d934573eaa
Webber <webber@takken.io>
2021-09-15 02:30:25 +0200

    Remove an animation that breaks on mobile.

--- src/components/section/ProfileCardSection/ProfileCardSection.tsx
+++ src/components/section/ProfileCardSection/ProfileCardSection.tsx
@@ -40,67 +40,60 @@ const ProfileCardSection = ({ section }: Props): JSX.Element => {
         backgroundSize: 'cover',
       }}
     >
-      <FadeIntoView>
-        <div className="max-w-4xl flex items-center h-auto flex-wrap mx-auto my-32 lg:my-0">
-          <div
-            id="profile"
-            className="w-full lg:w-3/5 rounded-lg lg:rounded-l-lg lg:rounded-r-none shadow-2xl bg-white opacity-75 mx-6 lg:mx-0"
-          >
-            <div className="p-4 md:p-12 text-center lg:text-left">
-              <div
-                className="block lg:hidden rounded-full shadow-xl mx-auto -mt-16 h-48 w-48 bg-cover bg-center"
-                style={{ backgroundImage: `url('${photoUrl}')` }}
-              />
-
-              <FadeIntoView delay={100}>
-                <h1 className="text-3xl font-bold pt-8 lg:pt-0">{title}</h1>
-                <div className="mx-auto lg:mx-0 w-4/5 pt-3 border-b-2 border-green-500 opacity-25" />
-              </FadeIntoView>
-
-              <FadeIntoView delay={200}>
-                <Vocation vocation={vocation} />
-              </FadeIntoView>
-
-              <FadeIntoView delay={250}>
-                <Location location={location} />
-              </FadeIntoView>
-
-              <FadeIntoView delay={300}>
-                <Bio shortDescription={shortDescription} />
-              </FadeIntoView>
-
-              {getInTouchLink && (
-                <FadeIntoView delay={500}>
-                  <GetInTouchButton
-                    text={getInTouchText}
-                    link={getInTouchLink}
-                  />
-                </FadeIntoView>
-              )}
-
-              <FadeIntoView delay={700}>
-                <div className="mt-6 pb-16 lg:pb-0 w-4/5 lg:w-full mx-auto flex flex-wrap items-center justify-between">
-                  {/* Use https://simpleicons.org/ to find the svg for your preferred product */}
-                  {twitterHandle && <TwitterIcon handle={twitterHandle} />}
-                  {facebookHandle && <FacebookIcon handle={facebookHandle} />}
-                  {instagramHandle && (
-                    <InstagramIcon handle={instagramHandle} />
-                  )}
-                  {youtubeHandle && <YoutubeIcon handle={youtubeHandle} />}
-                  {linkedInHandle && <LinkedInIcon handle={linkedInHandle} />}
-                </div>
-              </FadeIntoView>
-            </div>
-          </div>
-
-          <div className="w-full lg:w-2/5">
-            <img
-              className="rounded-none lg:rounded-lg shadow-2xl hidden lg:block"
-              src={photoUrl}
+      <div className="max-w-4xl flex items-center h-auto flex-wrap mx-auto my-32 lg:my-0">
+        <div
+          id="profile"
+          className="w-full lg:w-3/5 rounded-lg lg:rounded-l-lg lg:rounded-r-none shadow-2xl bg-white opacity-75 mx-6 lg:mx-0"
+        >
+          <div className="p-4 md:p-12 text-center lg:text-left">
+            <div
+              className="block lg:hidden rounded-full shadow-xl mx-auto -mt-16 h-48 w-48 bg-cover bg-center"
+              style={{ backgroundImage: `url('${photoUrl}')` }}
             />
+
+            <FadeIntoView delay={100}>
+              <h1 className="text-3xl font-bold pt-8 lg:pt-0">{title}</h1>
+              <div className="mx-auto lg:mx-0 w-4/5 pt-3 border-b-2 border-green-500 opacity-25" />
+            </FadeIntoView>
+
+            <FadeIntoView delay={200}>
+              <Vocation vocation={vocation} />
+            </FadeIntoView>
+
+            <FadeIntoView delay={250}>
+              <Location location={location} />
+            </FadeIntoView>
+
+            <FadeIntoView delay={300}>
+              <Bio shortDescription={shortDescription} />
+            </FadeIntoView>
+
+            {getInTouchLink && (
+              <FadeIntoView delay={500}>
+                <GetInTouchButton text={getInTouchText} link={getInTouchLink} />
+              </FadeIntoView>
+            )}
+
+            <FadeIntoView delay={700}>
+              <div className="mt-6 pb-16 lg:pb-0 w-4/5 lg:w-full mx-auto flex flex-wrap items-center justify-between">
+                {/* Use https://simpleicons.org/ to find the svg for your preferred product */}
+                {twitterHandle && <TwitterIcon handle={twitterHandle} />}
+                {facebookHandle && <FacebookIcon handle={facebookHandle} />}
+                {instagramHandle && <InstagramIcon handle={instagramHandle} />}
+                {youtubeHandle && <YoutubeIcon handle={youtubeHandle} />}
+                {linkedInHandle && <LinkedInIcon handle={linkedInHandle} />}
+              </div>
+            </FadeIntoView>
           </div>
         </div>
-      </FadeIntoView>
+
+        <div className="w-full lg:w-2/5">
+          <img
+            className="rounded-none lg:rounded-lg shadow-2xl hidden lg:block"
+            src={photoUrl}
+          />
+        </div>
+      </div>
     </div>
   )
 }
