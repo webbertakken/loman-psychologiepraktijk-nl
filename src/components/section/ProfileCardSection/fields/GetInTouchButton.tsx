83bcbff3e3e55e1b3d6bdfa17d7329e54238ef06
Webber <webber@takken.io>
2021-09-15 17:09:31 +0200

    Link around anchor tag instead of button

--- src/components/section/ProfileCardSection/fields/GetInTouchButton.tsx
+++ src/components/section/ProfileCardSection/fields/GetInTouchButton.tsx
@@ -7,13 +7,13 @@ interface Props {
 
 const GetInTouchButton = ({ link, text }: Props): JSX.Element => {
   return (
-    <Link href={link}>
-      <div className="pt-12 pb-8">
+    <div className="pt-12 pb-8">
+      <Link href={link}>
         <a className="bg-green-700 hover:bg-green-900 text-white font-bold py-2 px-4 rounded-full">
           {text}
         </a>
-      </div>
-    </Link>
+      </Link>
+    </div>
   )
 }
 
