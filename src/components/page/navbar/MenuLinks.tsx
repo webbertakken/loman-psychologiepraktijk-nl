95ab09b98f529e38774dc316c512fbad1d27d270
Webber <webber@takken.io>
2021-11-02 00:20:13 +0100

    menu style for desktop

--- src/components/page/navbar/MenuLinks.tsx
+++ src/components/page/navbar/MenuLinks.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import cx from 'classnames'
 
 interface Props {
   menu?: Record<string, any>[]
@@ -7,16 +8,21 @@ interface Props {
 const MenuLinks = ({ menu }: Props): JSX.Element => {
   return (
     <nav className="top-0 left-0 z-0 flex items-center justify-center w-full h-full py-5 -ml-0 space-x-5 text-base md:-ml-5 md:py-0 md:absolute">
-      {menu?.map(({ title, path }) => {
+      {menu?.map(({ title, path, isActive }) => {
         return (
           <Link key={path} href={path}>
             <a
               key={path}
-              className="relative font-medium leading-6 text-gray-600 transition duration-150 ease-out hover:text-gray-900"
+              className="group relative font-thin leading-6 text-gray-400 transition duration-150 ease-out hover:text-gray-400"
             >
-              <span className="block">{title}</span>
-              <span className="absolute bottom-0 left-0 inline-block w-full h-0.5 -mb-1 overflow-hidden">
-                <span className="absolute inset-0 inline-block w-full h-1 h-full transform bg-gray-900" />
+              <span className="block pb-1">{title}</span>
+              <span className="absolute bottom-0 left-0 inline-block w-full h-0.5 overflow-hidden">
+                <span
+                  className={cx(
+                    'absolute inset-0 inline-block w-full h-1/2 transform group-hover:bg-gray-400',
+                    { 'bg-gray-500': isActive }
+                  )}
+                />
               </span>
             </a>
           </Link>
--- src/components/page/navbar/NavBar.tsx
+++ src/components/page/navbar/NavBar.tsx
@@ -8,24 +8,24 @@ interface Props {
 
 const NavBar = ({ menu }: Props): JSX.Element => {
   return (
-    <section className="relative w-full px-8 text-gray-700 body-font lg:-mb-24">
+    <section className="relative w-full px-8 text-gray-300 body-font z-10">
       <div className="container flex flex-col flex-wrap items-center justify-between py-5 mx-auto md:flex-row max-w-7xl">
         <Link href="/">
-          <a>
+          <a className="z-10">
             <Logo className="h-14 select-none cursor-pointer" />
           </a>
         </Link>
 
         <MenuLinks menu={menu} />
 
-        <div className="relative z-10 inline-flex items-center space-x-3 md:ml-5 lg:justify-end">
-          <a
-            href="#"
-            className="inline-flex items-center justify-center px-4 py-2 text-base font-medium leading-6 text-gray-600 whitespace-no-wrap bg-white border border-gray-200 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:shadow-none"
-          >
-            🧑
-          </a>
-        </div>
+        {/*<div className="relative z-10 inline-flex items-center space-x-3 md:ml-5 lg:justify-end">*/}
+        {/*  <a*/}
+        {/*    href="#"*/}
+        {/*    className="inline-flex items-center justify-center px-4 py-2 text-base font-medium leading-6 text-gray-600 whitespace-no-wrap bg-white border border-gray-200 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:shadow-none"*/}
+        {/*  >*/}
+        {/*    🧑*/}
+        {/*  </a>*/}
+        {/*</div>*/}
       </div>
     </section>
   )
