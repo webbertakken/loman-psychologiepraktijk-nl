68328e4f2795429172ce4a3d5ab8b6b756e93bc1
Webber <webber@takken.io>
2021-11-10 01:07:14 +0100

    Upgrade to NextJS 12 and remove references to Babel

--- next.config.js
+++ next.config.js
@@ -1,14 +1,19 @@
 const withPlugins = require('next-compose-plugins')
 const withSvgr = require('next-svgr')
 
+/**
+ * @type {import('next').NextConfig}
+ **/
+const nextConfig = {
+  images: {
+    domains: ['images.ctfassets.net'],
+  },
+}
+
 module.exports = withPlugins(
   [
     withSvgr,
     // other plugins here
   ],
-  {
-    images: {
-      domains: ['images.ctfassets.net'],
-    },
-  }
+  nextConfig
 )
---
+++ test/components/Layout.test.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { render, screen, fireEvent } from '../testUtils'
+import Layout from '../../src/components/page/Layout'
+
+describe('Layout', () => {
+  it('matches snapshot', () => {
+    const { asFragment } = render(<Layout>{null}</Layout>, {})
+    expect(asFragment()).toMatchSnapshot()
+  })
+
+  it('renders renders children', () => {
+    render(<Layout><button onClick={() => alert('button is clickable')}>Test Button</button></Layout>, {})
+    window.alert = jest.fn()
+    fireEvent.click(screen.getByText('Test Button'))
+    expect(window.alert).toHaveBeenCalledWith('button is clickable')
+  })
+})
