96b201f33115fbf606a421b7cc95d4c4f1834196
Webber <webber@takken.io>
2021-12-20 23:52:54 +0100

    Replace page references with slugs

--- src/core/utils.ts
+++ src/core/utils.ts
@@ -32,3 +32,55 @@ export const omitPathRecursively = (
     })
   )
 }
+
+/**
+ * Replaces value at path inside an object recursively.
+ * A full path to the key is not required.
+ * The original object is not modified.
+ *
+ * Example:
+ *   const original = { a: { b: 'foo' } }
+ *   const replacer = (value) => value + 'bar'
+ *
+ *   replacePathsRecursively(original, 'b', replacer)
+ *   // output: { a: { b: 'foobar' } }
+ */
+export const replacePathsRecursively = (
+  original: object,
+  path: string | string[],
+  replacer: Function
+) => {
+  const paths = Array.isArray(path) ? path : [path]
+
+  let modified = original
+
+  paths.forEach((path) => {
+    modified = internal_replacePathRecursively(modified, path, replacer)
+  })
+
+  return modified
+}
+
+export const internal_replacePathRecursively = (
+  original: object,
+  path: string,
+  replacer: Function = (value, root) => undefined,
+  depth: number = 1
+) => {
+  const segments = path.split('.')
+  const final = depth === segments.length
+
+  return JSON.parse(
+    JSON.stringify(original, (key, value) => {
+      const match = key === segments[depth - 1]
+
+      if (!match) return value
+      if (!final)
+        return internal_replacePathRecursively(value, path, replacer, depth + 1)
+
+      const replacement = replacer(value)
+      if (!replacement || typeof replacement !== 'object') return replacement
+      return internal_replacePathRecursively(replacement, path, replacer)
+    })
+  )
+}
