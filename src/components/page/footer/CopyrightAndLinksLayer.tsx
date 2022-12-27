5d7db64bd466d64356311143f996387635e22afb
Webber <webber@takken.io>
2022-12-27 23:12:46 +0100

    feat: footer spacing and separators

--- src/components/page/footer/CopyrightAndLinksLayer.tsx
+++ src/components/page/footer/CopyrightAndLinksLayer.tsx
@@ -9,7 +9,8 @@ const CopyrightAndLinksLayer = ({}: Props): JSX.Element => {
 
   return (
     <div className="flex flex-col justify-between text-center lg:flex-row p-2 gap-2">
-      <div className="order-last text-sm text-gray-400 lg:order-first flex justify-center gap-1 flex-col md:flex-row">
+      <div className="order-last text-sm text-gray-400 lg:order-first flex justify-center gap-4 md:gap-1 flex-col md:flex-row">
+        <hr className="md:invisible my-4 border-gray-400/40" />
         <div>
           <strong>Loman Psychologiepraktijk</strong>
           <strong className="invisible md:visible"> |</strong>
@@ -21,8 +22,8 @@ const CopyrightAndLinksLayer = ({}: Props): JSX.Element => {
       <MenuItems
         menu={menu}
         ariaLabel="Footer navigation"
-        className="flex flex-col md:flex-row justify-center pb-0 -ml-4 -mr-4 text-sm"
-        itemClassName="mx-4 text-gray-400 hover:text-gray-300"
+        className="flex flex-col gap-4 md:flex-row justify-center pb-0 -ml-4 -mr-4 text-sm"
+        itemClassName="text-gray-400 hover:text-gray-300"
       />
     </div>
   );
