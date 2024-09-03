39ce95e6fe0473437c06b3a0e3aced8939e91994
Webber <webber@takken.io>
2024-09-03 14:14:15 +0200

    fix: error for unpublished banner

--- src/core/contentful.ts
+++ src/core/contentful.ts
@@ -6,5 +6,14 @@ export const getContentfulClient = (): ContentfulClientApi => {
     accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
   });
 
+  // client.getSpace()
+  //   .then((space) => {
+  //     console.log('Client initialized successfully.');
+  //     console.log('Space details:', space);
+  //   })
+  //   .catch((error) => {
+  //     console.error('Client initialization failed:', error);
+  //   });
+
   return client;
 };
