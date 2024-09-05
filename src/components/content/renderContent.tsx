2b064445f3e4eacca2456b2de5931586e0464b27
Webber Takken <webber@takken.io>
2024-09-05 18:36:30 +0200

    feat: add functionality for asset links (#23)
    * feat: add functionality for asset links
    
    * fix: only render h3 if it's set

--- src/components/content/renderContent.tsx
+++ src/components/content/renderContent.tsx
@@ -3,6 +3,8 @@ import { Document } from '@contentful/rich-text-types';
 import { DynamicIcon } from '../icons/DynamicIcon';
 import { fileTypeIcon } from './fileTypeIcon';
 import { humanFileSize } from '../../core/humanFileSize';
+import { PageProps } from '../../types/page';
+import Link from 'next/link';
 
 const options: Options = {
   renderNode: {
@@ -31,10 +33,27 @@ const options: Options = {
             className="shrink-0 grow-1 h-10 w-10 text-indigo-600 inline mb-2"
             aria-hidden="true"
           />
-          <span className=" text-sm text-center">{title}</span>
+          <span className="text-sm text-center">{title}</span>
           <span className="text-xs text-center"> ({humanFileSize(size, true)})</span>
         </a>
       );
+    },
+    'entry-hyperlink': function EntryHyperlink(node, children) {
+      const { target } = node.data;
+      const { fields, metadata, sys } = target || {};
+
+      // Link to a page
+      if (sys?.contentType?.sys?.id === 'page') {
+        const { title, slug } = fields as PageProps;
+        return (
+          <Link href={`/${slug}`} title={title}>
+            {children}
+          </Link>
+        );
+      }
+
+      console.warn('unhandled node:', node);
+      return <></>;
     },
     // 'embedded-entry-inline': function EmbeddedEntryInline(node, children) {
     //   console.log('node', node);
